9ac79952ccab272d286bf11d352372bc56592610
tapiwa <tapiwambizvo@gmail.com>
2024-10-18 12:01:10 +0200

    added tls stuff

--- keys.tf
+++ keys.tf
@@ -14,6 +14,11 @@ resource "tls_private_key" "private_instances_key" {
 resource "aws_key_pair" "public_instances_key_pair" {
     key_name = "public-ec2-key"
     public_key = tls_private_key.public_instances_key.public_key_openssh
+
+    provisioner "local-exec" {
+        command = "echo 'tls_private_key.private_instances_key.private_key_em' > ./public-ec2-key.pem "
+      
+    }
   
 }
 
@@ -22,3 +27,4 @@ resource "aws_key_pair" "private_instances_key_pair" {
     public_key = tls_private_key.private_instances_key.public_key_openssh
   
 }
+
