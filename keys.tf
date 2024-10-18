db7f7cc35c77193847ac39c073d51fb239c24dd8
tapiwa <tapiwambizvo@gmail.com>
2024-10-18 07:18:44 +0200

    private key logical name

--- keys.tf
+++ keys.tf
@@ -18,7 +18,7 @@ resource "aws_key_pair" "public_instances_key_pair" {
 }
 
 resource "aws_key_pair" "private_instances_key_pair" {
-    key_name = "public-ec2"
+    key_name = "private-ec2"
     public_key = tls_private_key.private_instances_key.public_key_openssh
   
 }
